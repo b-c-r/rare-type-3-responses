e4d554bf63e1cb1e96d688cafc615d8010f8ed82
b-c-r <bcr-ecostats+github@mailbox.org>
2023-02-08 22:33:23 +0100

    Updated comments and references

--- FUNCTIONS/calc_gen_fr.R
+++ FUNCTIONS/calc_gen_fr.R
@@ -1,6 +1,7 @@
 ################################################################################
-#    Function "calc_gen_fr" to calculate FR curves                             #
-#    Copyright (C) 2023 Björn C. Rall                                          #
+#    Function "calc_gen_fr" to calculate functional response curves            #
+#                                                                              #
+#    Copyright (C) 2023 Björn C. Rall (b-c-r@mailbox.org)                      #
 #                                                                              #
 #    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
@@ -17,22 +18,25 @@
 ################################################################################
 
 # Description:
-# The function calculates the shapes of the generalized or Theta-sigmoid
+# The function calculates the shapes of the generalized or theta-sigmoid
 # functional response (e.g., Real 1977; Okuyama & Ruyle 2011; Vucic-Pestic et
-# al. 2010). See Kalinkat et al. (under review) for details.
+# al. 2010). See Kalinkat et al. (2023) and Rall et al. (2023) for details.
 #
 # Arguments:
 # N:     The resource densities (positive vector)
 # Fmax:  The maximum feeding rate (the curves' asymptote, single value)
 # N0:    The half saturation rate (density at which Fmax is reached, single value)
-# theta: The shaping exponent (can be a vector)
+# Theta: The shaping exponent (can be a vector). Theta = 1 is a type II functional
+#            response. Theta > 1 is a sigmoid type III functional response. Most
+#            studies use Theta = 2 for modeling a type III functional response.
 # 
 # Requirements:
 # None
 # 
 # References
-# Kalinkat et al. (under review): Empirical evidence of type III functional responses and why it remains rare.
+# Kalinkat et al. (2023): Empirical evidence of type III functional responses and why it remains rare. Front Ecol Evol 11: 1033818. https://doi.org/10.3389/fevo.2023.1033818
 # Okuyama & Ruyle (2011):Solutions for functional response experiments. Acta Oecologia 37: 512-516. https://doi.org/10.1016/j.actao.2011.07.002
+# Rall et al. (2023): Rare type III responses: Code & modelling methods (v1.0.0). Zenodo; https://doi.org/10.5281/zenodo.7619822
 # Real (1977): The kinetics of functional response. Am Nat 111: 289-300. https://doi.org/10.1086/283161
 # Vucic-Pestic et al. (2010): Allometric functional response model: body masses constrain interaction strengths. J Anim Ecol: 79: 249-256. https://doi.org/10.1111/j.1365-2656.2009.01622.x
 #
@@ -43,7 +47,7 @@ calc_gen_fr <- function(N = seq(0, 20, length=1000),
                         Theta = c(1, 1.25, 1.5, 2, 3)){
   
   out <- foreach::foreach(i = 1:length(Theta), .combine = cbind) %do% {
-    Fmax * N^Theta[i] / (N0^Theta[i] + N^Theta[i])
+    Fmax * N^Theta[i] / (N0^Theta[i] + N^Theta[i])                              # the functional response model
   }
   
   out <- as.data.frame(cbind(N, out))

47bfefb44c4dd9f4c306d4e7d7cc63ea3d636a12
b-c-r <bcr-ecostats+github@mailbox.org>
2023-02-08 21:55:03 +0100

    Updated references and comments

--- FUNCTIONS/run_foodchain_sim.R
+++ FUNCTIONS/run_foodchain_sim.R
@@ -2,7 +2,7 @@
 #    Function "run_foodchain_sim" to run the 3-species food chain across       #
 #    a gradient of q values                                                    #
 #                                                                              #
-#    Copyright (C) 2023 Björn C. Rall                                          #
+#    Copyright (C) 2023 Björn C. Rall (b-c-r@mailbox.org)                      #
 #                                                                              #
 #    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
@@ -19,32 +19,33 @@
 ################################################################################
 
 # Description:
-# The function simulates the 3-species food chain across a gradient of q values
+# The function simulates the 3-species food chain across a gradient of q-values
 # and saves the output as *.csv files for a bifurcation diagram. The food-chain
-# model is based on Otto et al. (2007) with adaptions from Williams and Martinez
-# (2004). See also the supplement information of Kalinkat et al. (under review)
-# for details. The package odeintr (Keitt 2007) is required to run the function.
+# model is based on Otto et al. (2007) combined with ideas from Williams and
+# Martinez (2004). See also Kalinkat et al. (2023) and Rall et al. (2023) for
+# details. The package odeintr (Keitt 2017) is required to run the function.
 #
 # Arguments:
-# qrange:       a vector with all q-values to be simulated (default = seq(0, .2, length = 501)
-# ts_length:    the length of each time series to be simulated (default = 100000)
-# steplength:   the explicit times steps that will be simulated (default = .5)
-# analyze_ts:   the percentage of the time series that should be analyzed (e.g.
-#               the last 5 %) (default = 0.05; between 0 and 1)
-# unique_out:  Should double entries within one q value be deleted? (default = T),
-# max_out:     Number of different extrema to be saved (default = 20; 0 == All are save),
-# output_path: the (sub-)path were the output files should be saved to (default = "SIM_OUT/")
+# qrange:        A vector with all q-values to be simulated (default = seq(0, .2, length = 501)
+# ts_length:     The length of each time series to be simulated (default = 100000)
+# steplength:    The explicit times steps that will be simulated (default = .5)
+# analyze_ts:    The percentage of the time series that should be analyzed (e.g.
+#                    the last 5 % [0.05]) (default = 0.05; between 0 and 1)
+# unique_out:    Should double entries within one q value be deleted? (default = T),
+# max_out:       Number of different extrema to be saved (default = 20; 0 == saves all),
+# output_path:   The (sub-)path were the output files should be saved to (default = "SIM_OUT/")
+# create.folder: Should the function create an output folder? Default = T.
 # 
 # Requirements:
 # Compilation of the food chain model (run compile_foodchain())
-# Needs the package odeintr (https://CRAN.R-project.org/package=odeintr)
+# Needs the package odeintr (https://CRAN.R-project.org/package=odeintr, Keitt 2017)
 # 
 # References
-# Kalinkat et al. (under review): Empirical evidence of type III functional responses and why it remains rare.
-# Keitt (2007): odeintr: C++ ODE Solvers Compiled on-Demand, 1.71, https://CRAN.R-project.org/package=odeintr
+# Kalinkat et al. (2023): Empirical evidence of type III functional responses and why it remains rare. Front Ecol Evol 11: 1033818. https://doi.org/10.3389/fevo.2023.1033818
+# Keitt (2017): odeintr: C++ ODE Solvers Compiled on-Demand, 1.71; https://CRAN.R-project.org/package=odeintr
 # Otto et al. (2007): Allometric degree distributions facilitate food-web stability. Nature 450: 1226-1229; https://doi.org/10.1038/nature06359.
-# Williams and Martinez (2004): Stabilization of chaotic and non-permanent food-web dynamics. Eur Phys J B 38: 297-303; https://doi.org10.1140/epjb/e2004-00122-1.
-#
+# Rall et al. (2023): Rare type III responses: Code & modelling methods (v1.0.0). Zenodo; https://doi.org/10.5281/zenodo.7619822
+# Williams and Martinez (2004): Stabilization of chaotic and non-permanent food-web dynamics. Eur Phys J B 38: 297-303; https://doi.org10.1140/epjb/e2004-00122-1.#
 
 run_foodchain_sim <- function(qrange = seq(0, .2, length = 501),
                               ts_length = 100000,
@@ -54,6 +55,8 @@ run_foodchain_sim <- function(qrange = seq(0, .2, length = 501),
                               max_out = 20,
                               output_path = "SIM_OUT/",
                               create.folder = T){
+
+  ## check if the output folder exists, if not create it:
   if(create.folder){
     if(!dir.exists(output_path)){
       dir.create(output_path)
@@ -75,13 +78,13 @@ run_foodchain_sim <- function(qrange = seq(0, .2, length = 501),
     set_foodchain_parms(q = qrange[i])
     
     ## run the simulation
-    out <- chainC_adap(c(runif(1, .1, 1),
-                         runif(1, .1, 1),
-                         runif(1, .1, 1)),
+    out <- chainC_adap(c(runif(1, .1, 1),                                       # random initial biomass densities (basal)
+                         runif(1, .1, 1),                                       # random initial biomass densities (intermediate)
+                         runif(1, .1, 1)),                                      # random initial biomass densities (top)
                        ts_length,
                        steplength)
     
-    ## cut the time series for further analyses
+    ## cut the time series for further analyses and save only the last x pecent:
     out <- out[out$Time > max(out$Time)*(1-analyze_ts),]
     
     ## save the population extrema locally

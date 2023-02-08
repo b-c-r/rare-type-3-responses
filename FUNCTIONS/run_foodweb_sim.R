c824400a5164ca3c547ec0e5bfe7b301faf3d386
b-c-r <bcr-ecostats+github@mailbox.org>
2023-02-08 21:00:23 +0100

    Updated references and comments

--- FUNCTIONS/run_foodweb_sim.R
+++ FUNCTIONS/run_foodweb_sim.R
@@ -2,7 +2,7 @@
 #    Function "run_foodchain_sim" to run the 10-species food web across        #
 #    a gradient of q values                                                    #
 #                                                                              #
-#    Copyright (C) 2023 Björn C. Rall                                          #
+#    Copyright (C) 2023 Björn C. Rall (b-c-r@mailbox.org)                      #
 #                                                                              #
 #    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
@@ -19,39 +19,37 @@
 ################################################################################
 
 # Description:
-# The function simulates the 10-species food web across a gradient of q values
-# and saves the output as *.csv files. The foodweb model is based on Williams &
-# Martinez (2004) paper with minor changes. See also the supplement information
-# of Kalinkat et al. (under review) for details. The package odeintr
-# (Keitt 2007) is required to run the function.
+# The function simulates the 10-species food web across a gradient of q-values
+# and saves the output as *.csv files. The foodweb model is based on Williams and
+# Martinez (2004) paper with minor changes. See also Kalinkat et al. (2023) and 
+# Rall et al. (2023) for details. The package odeintr (Keitt 2017) is required
+# to run the function.
 #
 # Arguments:
 # qrange:          a vector with all q-values to be simulated (default = seq(0, 4, length = 201)
-# a:               the allometric constant (default = 0.2227; see Otto et al. 2008)
-# b:               the allometric exponent (default = -0.25; see Otto et al. 2008)
-# e:               the assimilation efficiency (default = 0.85; see Otto et al. 2008)
-# y:               the relative maximum feeding rate (default = 8; see Otto et al. 2008)
-# N0:              the half saturation rate (default = 0.5; see Otto et al. 2008)
-# R:               the body mass ratio (default = c(10, 100))
+# a:               the allometric constant (default = 0.2227; see Otto et al. 2007)
+# b:               the allometric exponent (default = -0.25; see Otto et al. 2007)
+# e:               the assimilation efficiency (default = 0.85; see Otto et al. 2007)
+# y:               the relative maximum feeding rate (default = 8; see Otto et al. 2007)
+# N0:              the half saturation rate (default = 0.5; see Otto et al. 2007)
+# R:               the body mass ratio range a species can have (default = c(10, 100)). Must be a vector of 2 numbers!
 # ts_runs:         the number of sequential simulation runs adding up to on time series (default = 10)
 # ts_run_length:   the length of one separated simulation from all runs adding up to one time series (default = 500)
 # steplength:      the explicit times steps that will be simulated (default = 1)
 # output_path:     the (sub-)path were the output files should be saved to (default = "SIM_OUT/")
 # output_filename: the name of the output file (default = "strong_interactions.csv")
-# create_csv:      logical: should a csv be written to teh hard-drive? (default = T)
+# create_csv:      logical: should a csv be written to the hard-drive? (default = T)
 # create_folder:   logical: should a new path be generated (default = T)
-#
-# 
-# The default settings are as used in Kalinkat et al. (under review).
 # 
 # Requirements:
-# Compilation of the foodwe model (run compile_foodweb())
-# Needs the package odeintr (https://CRAN.R-project.org/package=odeintr)
+# Compilation of the foodweb model (run compile_foodweb())
+# Needs the package odeintr (https://CRAN.R-project.org/package=odeintr; Keitt 2017)
 # 
 # References
-# Kalinkat et al. (under review): Empirical evidence of type III functional responses and why it remains rare.
-# Keitt (2007): odeintr: C++ ODE Solvers Compiled on-Demand, 1.71, https://CRAN.R-project.org/package=odeintr
+# Kalinkat et al. (2023): Empirical evidence of type III functional responses and why it remains rare. Front Ecol Evol 11: 1033818. https://doi.org/10.3389/fevo.2023.1033818
+# Keitt (2017): odeintr: C++ ODE Solvers Compiled on-Demand, 1.71; https://CRAN.R-project.org/package=odeintr
 # Otto et al. (2007): Allometric degree distributions facilitate food-web stability. Nature 450: 1226-1229; https://doi.org/10.1038/nature06359.
+# Rall et al. (2023): Rare type III responses: Code & modelling methods (v1.0.0). Zenodo; https://doi.org/10.5281/zenodo.7619822
 # Williams and Martinez (2004): Stabilization of chaotic and non-permanent food-web dynamics. Eur Phys J B 38: 297-303; https://doi.org10.1140/epjb/e2004-00122-1.
 #
 
@@ -69,6 +67,8 @@ run_foodweb_sim <- function(qrange = seq(0, 4, length = 201),
                             output_filename = "strong_interactions.csv",
                             create_csv = T,
                             create.folder = T){
+  
+  ## check if the output folder exists, if not create it:
   if(create.folder){
     if(!dir.exists(output_path)){
       dir.create(output_path)
@@ -76,8 +76,9 @@ run_foodweb_sim <- function(qrange = seq(0, 4, length = 201),
     }
   }
   
-  divs <- c()
+  divs <- c()                                                                   ## object to store the current biodiversity levels
   
+  ## run the simulations across the qrange vector:
   for(i in 1:length(qrange)){
     
     ## setup the model parameter values
@@ -91,32 +92,32 @@ run_foodweb_sim <- function(qrange = seq(0, 4, length = 201),
     
     ## run the simulation
     for(j in 1:ts_runs){
-      out <- NULL
-      if(j == 1){
-        init <- runif(10, .1, 1)
+      out <- NULL                                                               ## a clean object to store the simulation output
+      if(j == 1){                                                               ## is this the initial simulation run?
+        init <- runif(10, .1, 1)                                                ## ten initial biomass densities (only for the first run of the time series)
       }
-      out <- foodweb(init, ts_run_length, steplength)
-      for(k in 1:10){
+      out <- foodweb(init, ts_run_length, steplength)                           ## simulate the partial time series
+      for(k in 1:10){                                                           ## check if the any species is extinct (extinction if biomass density is smaller than 1e-10)
         if(any(out[,(k+1)] < 1e-10)){
-          init[k] <- 0
+          init[k] <- 0                                                          ## if a species is extinct, set the initial biomass density to "0"
         } else{
-          init[k] <- out[nrow(out),(k+1)]
+          init[k] <- out[nrow(out),(k+1)]                                       ## if a species is alive, set the initial biomass density to the last value of the current time series
         }
       }
     }
     
-    ## count the biodiversity
-    cumdiv <- 0
-    for(j in 1:10){
-      if(any(out[,(j+1)] > 1e-10)){
-        cumdiv <- cumdiv + 1
+    ## count the biodiversity (at the end of the simulation)
+    cumdiv <- 0                                                                 ## the cumulative species richness (starting at 0)
+    for(j in 1:10){                                                             ## loop over all 10 species
+      if(any(out[,(j+1)] > 1e-10)){                                             ## check if the species is alive (> 1e-10)
+        cumdiv <- cumdiv + 1                                                    ## add up if the species is alive
       }
     }
-    divs[i] <- cumdiv
+    divs[i] <- cumdiv                                                           ## transfer the diversity of the current simulation (cumdiv) to the overall diverstiy vector
     
   }
   
-  ## save the biodiversity results to file (hard-drive)
+  ## output
   if(create_csv){
     write.csv(
       data.frame(q = qrange,
@@ -124,11 +125,11 @@ run_foodweb_sim <- function(qrange = seq(0, 4, length = 201),
       ),
       paste(output_path, output_filename, sep =""),
       row.names = F
-    ) 
+    )                                                                           ## save the biodiversity results to a file (on the hard-drive)
   } else{
     return(data.frame(q = qrange,
                       div = divs
-    ))
+    ))                                                                          ## returns the biodiversity results to the R environment
   }
 }
 

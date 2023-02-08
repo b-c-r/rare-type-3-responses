120088502c3e0b0c854b7105fab9ecb736a52922
b-c-r <bcr-ecostats+github@mailbox.org>
2023-02-08 22:37:12 +0100

    Added seed; updated comments and references

--- generate_figure_01.R
+++ generate_figure_01.R
@@ -1,8 +1,8 @@
 ################################################################################
 #    Script for creating figure 1 for "Empirical evidence of type III          #
-#    functional responses and why it remains rare"                             #
+#    functional responses and why it remains rare" by Kalinkat et al. (2023)   #
 #                                                                              #
-#    Copyright (C) 2023 Björn C. Rall                                          #
+#    Copyright (C) 2023 Björn C. Rall (b-c-r@mailbox.org)                      #
 #                                                                              #
 #    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
@@ -18,6 +18,16 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.    #
 ################################################################################
 
+################################################################################
+## Overview                                                                   ##
+################################################################################
+
+# With this script you can re-create figure 1 from Kalinkat et al. (2023). For
+# more details on the methodology see Rall et al. (2023).
+#
+# Kalinkat et al. (2023): Empirical evidence of type III functional responses and why it remains rare. Front Ecol Evol 11: 1033818. https://doi.org/10.3389/fevo.2023.1033818
+# Rall et al. (2023): Rare type III responses: Code & modelling methods (v1.0.0). Zenodo; https://doi.org/10.5281/zenodo.7619822
+#
 
 ################################################################################
 ## setup                                                                      ##
@@ -41,81 +51,88 @@ for(i in 1:length(fnames)){
   source(paste(path, fnames[i], sep = ""))                                      # reads all source files
 }
 
+## set a seed for reproducibility
+set.seed(111)
+
 ################################################################################
-## simulations for Figure 1E/F from Kalinkat et al. (under review)            ##
+## simulations for Figure 1E/F from Kalinkat et al. (2023)                    ##
 ################################################################################
 
 ## run the food chain simulations for the bifurcation diagram
+## (see https://doi.org/10.5281/zenodo.7619822 for details on settings)
 system.time({
-  run_foodchain_sim_mt(qrange = seq(0, .2, length = 1800),
-                       ts_length = 150000,
-                       steplength = .5,
-                       analyze_ts = 0.05,
-                       unique_out = T,
+  run_foodchain_sim_mt(qrange = seq(0, .2, length = 1800),                      # range of q-value
+                       ts_length = 150000,                                      # total length of each timeseries
+                       steplength = .5,                                         # initial step length
+                       analyze_ts = 0.05,                                       # the range of the time series that should be analyzed (here the last 5%)
+                       unique_out = T,                                          # only unique extrema should be saved
                        max_out = 0,                                             # saves all extrema, leads to large out files (each >6MB)
-                       output_path = "SIM_OUT/",
-                       noC = 12)
-}) # ~ 4-7min with AMD Ryzen 6000 series laptop
+                       output_path = "SIM_OUT/",                                # saves the output files in "SIM_OUt/" - will be generated if not existing
+                       noC = 16)                                                # Number of cores that should be used. Use parallel::detectCores() to see how many threads you have. Consider to not use all if you need your computer in the meanwhile.
+})
 
 ## run the food web simulations for the strong interactions biodiversity diagram
+## (see https://doi.org/10.5281/zenodo.7619822 for details on settings)
 system.time({
-  run_foodweb_sim_mt(qrange = seq(0, 4, length = 600),
-                     a = 0.2227,
-                     b = -0.25,
-                     e = 0.85,
-                     y = 8,
-                     N0 = .5,
-                     Rrange = c(10,100),
-                     ts_runs = 20,
-                     ts_run_length = 1000,
-                     steplength = .5,
-                     output_path = "SIM_OUT/",
-                     output_filename = "strong_interactions.csv",
-                     noC = 12)
-}) # ~ 9-11 min with AMD Ryzen 6000 series laptop
+  run_foodweb_sim_mt(qrange = seq(0, 4, length = 600),                          # range of q-value
+                     a = 0.2227,                                                # the allometric constant
+                     b = -0.25,                                                 # the allometric exponent
+                     e = 0.85,                                                  # the assimilation efficiency
+                     y = 8,                                                     # the relative maximum feeding rate
+                     N0 = .5,                                                   # the half saturation density
+                     Rrange = c(10,100),                                        # the range of (average) body mass ratios a consumer species in the food web can have
+                     ts_runs = 20,                                              # number of sub-timeseries that shoudl be analyzed. 
+                     ts_run_length = 1000,                                      # total length of each sub-timeseries. e.g. 20 sub-timeseries with each 1000 time-steps sum up to a 20,000 timesteps timeseries.
+                     steplength = .5,                                           # the steplength-length. Here 2000 steps of length of 0.5 create 1000 time steps.
+                     output_path = "SIM_OUT/",                                  # saves the output files in "SIM_OUt/" - will be generated if not existing
+                     output_filename = "strong_interactions.csv",               # output file name
+                     noC = 16)                                                  # Number of cores that should be used. Use parallel::detectCores() to see how many threads you have. Consider to not use all if you need your computer in the meanwhile.
+})
 
 ## run the food web simulations for the weak interactions biodiversity diagram
+## (see https://doi.org/10.5281/zenodo.7619822 for details on settings)
 system.time({
-  run_foodweb_sim_mt(qrange = seq(0, 4, length = 600),
-                     a = 0.2227,
-                     b = -0.25,
-                     e = 0.85,
-                     y = 4,
-                     N0 = 1,
-                     Rrange = c(10,100),
-                     ts_runs = 20,
-                     ts_run_length = 1000,
-                     steplength = .5,
-                     output_path = "SIM_OUT/",
-                     output_filename = "weak_interactions.csv",
-                     noC = 12)
-}) # ~ 2-3 min with AMD Ryzen 6000 series laptop
+  run_foodweb_sim_mt(qrange = seq(0, 4, length = 600),                          # range of q-value
+                     a = 0.2227,                                                # the allometric constant
+                     b = -0.25,                                                 # the allometric exponent
+                     e = 0.85,                                                  # the assimilation efficiency
+                     y = 4,                                                     # the relative maximum feeding rate
+                     N0 = 1,                                                    # the half saturation density
+                     Rrange = c(10,100),                                        # the range of (average) body mass ratios a consumer species in the food web can have
+                     ts_runs = 20,                                              # number of sub-timeseries that shoudl be analyzed. 
+                     ts_run_length = 1000,                                      # saves the output files in "SIM_OUt/" - will be generated if not existing
+                     steplength = .5,                                           # the steplength-length. Here 2000 steps of length of 0.5 create 1000 time steps.
+                     output_path = "SIM_OUT/",                                  # saves the output files in "SIM_OUt/" - will be generated if not existing
+                     output_filename = "weak_interactions.csv",                 # output file name
+                     noC = 16)                                                  # Number of cores that should be used. Use parallel::detectCores() to see how many threads you have. Consider to not use all if you need your computer in the meanwhile.
+})
 
 
 ################################################################################
-## generate figure 01 with all 6 subplots from Kalinkat et al. (under review) ##
+## generate figure 01 with all 6 subplots from Kalinkat et al. (2023        ) ##
 ################################################################################
 
 ## pdf setup
-dir.create("FIG_OUT/")
+dir.create("FIG_OUT/")                                                          # creates an empty directory for the output pdf
 pdf(file = "FIG_OUT/Figure01.pdf",
     width = 7,
     height = 4.8,
-    title = "Figure 1: Rare Type III Responses")
+    title = "Figure 1: Rare Type III Responses")                                # pdf settings, see ?pdf for details
 
 ## Graphical settings
 par(mfrow = c(2,3),
     oma = c(.1,.1,.1,.1),
     mar = c(4,4,.5,.5),
-    las = 1)
+    las = 1)                                                                    # graphical settings to create a nice 2*3 ploting area
 
 ## create the figure
+# calls functions to create respective sub-plots
 create_fig01A(save_output = F)
 create_fig01B(save_output = F)
-create_fig01C_alt(save_output = F)
+create_fig01C(save_output = F)
 create_fig01D(save_output = F)
 create_fig01E(save_output = F)
 create_fig01F(save_output = F)
-dev.off()
+dev.off()                                                                       # closes pdf
 
-writeLines(capture.output(sessionInfo()), "sessionInfo.txt")
+writeLines(capture.output(sessionInfo()), "sessionInfo.txt")                    # saves information on your R installation for reproducibility
